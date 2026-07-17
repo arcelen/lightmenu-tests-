ac1d4f4d694f70edd8fa7301e66e5a7290ee1b10
Arcelen <140386681+arcelen@users.noreply.github.com>
2026-07-17 17:34:54 +0200

    add homepage tests

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,10 +1,22 @@
-import { test, expect} from '@playwright/test'
-test('homepage loads', async ({ page }) => {
+import {test, expect} from '@playwright/test';
+
+test('homepage loads', async ({page}) =>{
     await page.goto('https://lightmenu.app');
     await expect(page).toHaveTitle(/.+/);
 });
 
-test('page is visible', async ({ page }) => {
+test ('page is visible', async ({page}) =>{
     await page.goto('https://lightmenu.app');
     await expect(page.locator('body')).toBeVisible();
 });
+
+test ('sign in is visible', async ({page}) => {
+    await page.goto('https://lightmenu.app');
+    await expect(page.locator('text=Sign in')).toBeVisible();
+});
+
+test ('sign up button is visible' , async ({page}) => {
+    await page.goto('https://lightmenu.app');
+    await expect(page.locator('text=Get started free').first()).toBeVisible();
+});
+
